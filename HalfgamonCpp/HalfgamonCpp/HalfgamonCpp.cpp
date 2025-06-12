51192234c4a18b8a19b0c7bb623d6a94262c719b
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-06-13 06:57:41 +0900

    0021 ◎ C 06/12 06/13 0.001│ToDo│100対戦行って、勝率表示

--- HalfgamonCpp/HalfgamonCpp/HalfgamonCpp.cpp
+++ HalfgamonCpp/HalfgamonCpp/HalfgamonCpp.cpp
@@ -26,28 +26,43 @@ int main()
 		bd.print();
 	}
 	if( true ) {
-		char next = BLACK;
-		uchar black_AI = AI_TYPE_PMC;
-		uchar white_AI = AI_TYPE_RANDOM;
-		for(int i = 0;;++i) {
-			int d1, d2;
-			roll_dice(d1, d2);
-			cout << "#" << (i+1) << ": dice = (" << d1 << ", " << d2 << ")" << endl;
-			if( next == BLACK ) {
-				auto mvs = bd.sel_move(black_AI, BLACK, d1, d2);
-				cout << "mvs = " << to_str(mvs) << endl;
-				bd.do_move(next, mvs);
-			} else {
-				auto mvs = bd.sel_move(black_AI, WHITE, d1, d2);
-				cout << "mvs = " << to_str(mvs) << endl;
-				bd.do_move(next, mvs);
+		int n_black_win = 0;
+		int n_white_win = 0;
+		for(int k = 0; k < 100; ++k) {
+			cout << "#" << (k+1) << ":" << endl;
+			Board bd;
+			char next = BLACK;
+			uchar black_AI = AI_TYPE_PMC;
+			uchar white_AI = AI_TYPE_RANDOM;
+			for(int i = 0;;++i) {
+				int d1, d2;
+				roll_dice(d1, d2);
+				//cout << "#" << (i+1) << ": dice = (" << d1 << ", " << d2 << ")" << endl;
+				if( next == BLACK ) {
+					auto mvs = bd.sel_move(black_AI, BLACK, d1, d2);
+					//cout << "mvs = " << to_str(mvs) << endl;
+					bd.do_move(next, mvs);
+				} else {
+					auto mvs = bd.sel_move(black_AI, WHITE, d1, d2);
+					//cout << "mvs = " << to_str(mvs) << endl;
+					bd.do_move(next, mvs);
+				}
+				//bd.print();
+				if( bd.m_pip_black == 0 ) {
+					++n_black_win;
+					break;
+				}
+				if( bd.m_pip_white == 0 ) {
+					++n_white_win;
+					break;
+				}
+				next = (BLACK+WHITE) - next;
+				//getchar();
 			}
 			bd.print();
-			if( bd.m_pip_black == 0 || bd.m_pip_white == 0 )
-				break;
-			next = (BLACK+WHITE) - next;
-			//getchar();
 		}
+		cout << "black won = " << n_black_win << endl;
+		cout << "white won = " << n_white_win << endl;
 	}
 
     std::cout << "\nOK.\n";
